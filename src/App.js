c9ffe4ca18c0b19ed8cb8a71f78a3e8dc8e59f56
Sean Hyde <sean.hyde04@gmail.com>
2024-03-10 18:13:28 -0400

    Welcome Page finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,99 @@
-import logo from './logo.svg';
 import './App.css';
+import wordleLogo from './assets/wordleLogo.png'; // Import the image
+import React from 'react';
+
 
 function App() {
+
+  function returnTodaysDate(){
+    let today = new Date();
+    let month = today.getMonth()+1;
+
+    switch(month){
+      case 1:
+        month = "January";
+        break;
+      case 2:
+        month = "February";
+        break;
+      case 3:
+        month = "March";
+        break;
+      case 4:
+        month = "April";
+        break;
+      case 5:
+        month = "May";
+        break;
+      case 6:
+        month = "June";
+        break;
+      case 7:
+        month = "July";
+        break;
+      case 8:
+        month = "August";
+        break;
+      case 9:
+        month = "September";
+        break;
+      case 10:
+        month = "October";
+        break;
+      case 11:
+        month = "November";
+        break;
+      case 12:
+        month = "December";
+        break;
+      default:
+        month = "January";
+    }
+    
+    let date = month + " " +today.getDate() + ", " + today.getFullYear();
+    return date;
+  }
+
+  function returnNumOfWordleGames(){
+    let numOfWordleGames = 12122;
+    return numOfWordleGames;
+  }
+
+  function returnWordleLogo(){
+    return <img src={wordleLogo} alt="Wordle Logo" />;  }
+
+  function howToPlayOnPress(){
+    console.log("How to Play button pressed");}
+
+  function logInOnPress(){
+      console.log("Log In button pressed");
+  }
+
+  function startGameOnPress(){
+    console.log("Start Game button pressed");
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="welcome-page">
+      {returnWordleLogo()}
+      <h1>Wordle</h1>
+      <p> Get 6 chances to guess a 5-letter word.</p>
+
+
+      <div className = "welcome-page-button-container">
+        <button onClick = {howToPlayOnPress}className = "how-to-play" > How to Play</button>
+        <button className = "log-in">Log In </button>
+        <button className = "start-game">Start Game</button>
+      </div>
+
+      <div className = "welcome-page-footer">
+      <h1> {returnTodaysDate()} </h1>
+      <p> No. {returnNumOfWordleGames()} </p>
+      <p> Wordle Clone by Sean Hyde</p>
+      </div>
+
     </div>
   );
 }
